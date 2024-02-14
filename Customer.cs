19bbfd3a0ba42a9d5b8f09504922554f82287d46
John-Glatz <156080749+John-Glatz@users.noreply.github.com>
2024-02-13 23:20:53 -0500

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Johnathon_Glatz_Unit6_IT481
+{
+    class Customer
+    {
+        int NumberOfItems;
+
+        public Customer() 
+        {
+            NumberOfItems = 6;
+        }
+
+        public Customer(int items)
+        {
+            int ClothingItems = items;
+
+            if (ClothingItems == 0)
+            {
+                NumberOfItems = GetRandomNumber(1, 20);
+            }
+            else
+            {
+                NumberOfItems = ClothingItems;
+            }
+        }
+        public int getNumberOfItems()
+        {
+            return NumberOfItems;
+        }
+
+        private static readonly Random getrandom = new Random();
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (getrandom)
+            {
+                return getrandom.Next(min, max);
+            }
+        }
+    }
+}
